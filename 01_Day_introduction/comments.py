1c82b0840b78c0018e960e296e4aff7a3474b5cc
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-07-11 21:09:54 +0500

    Create comments.py

---
+++ 01_Day_introduction/comments.py
@@ -0,0 +1,15 @@
+# Comments
+# Comments play a crucial role in enhancing code readability and allowing developers to leave notes within their code. 
+# In Python, any text preceded by a hash (#) symbol is considered a comment and is not executed when the code runs.
+
+# 1) Example: Single Line Comment
+    # This is the first comment
+    # This is the second comment
+    # Python is eating the world
+
+# 2) Multiline Comment
+# Triple quote can be used for multiline comment if it is not assigned to a variable
+"""This is multiline comment
+multiline comment takes multiple lines.
+python is eating the world
+"""
