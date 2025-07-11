0ec3d35a897fa0bce308c228af29d276ea6f40c3
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-07-11 21:28:18 +0500

    Update helloworld.py

--- 01_Day_introduction/helloworld.py
+++ 01_Day_introduction/helloworld.py
@@ -1,2 +1,26 @@
 print("Hello World!")
 print("Weolcome to Python Programming Language")
+
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print(4 + 2)   # addition(+)
+print(4 - 2)   # subtraction(-)
+print(4 * 2)   # multiplication(*)
+print(4 / 2)   # division(/)
+print(4 ** 2)  # exponential(**)
+print(4 % 2)   # modulus(%)
+print(4 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asif'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asif'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
