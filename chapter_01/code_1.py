ecd196b775571775a5edddacba9bb2a49c192b17
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-08-22 03:40:58 +0500

    Create code_1.py

---
+++ chapter_01/code_1.py
@@ -0,0 +1,23 @@
+print("hello World")
+print("""
+Welcome to Python Programming Language")
+Python is a powerful programming language")
+Python is a versatile programming language
+Python is a easy to learn programming language
+""")
+
+# this is a comment
+'''
+this is a multi line comment
+'''  
+"""
+this is a multi line comment
+"""
+
+# this is a variable
+name: str = "MUHAMMAD ASIF"
+age : int = 20
+print(f"My name is {name} and my age is {age}") # on this line we are using f-string to print the value of name and age
+print("My name is ",name )
+print("My age is ",age)
+
