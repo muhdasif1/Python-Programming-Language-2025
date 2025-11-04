890dd67920d443c150fecc29e6db4454ce78910b
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-11-04 20:52:33 +0500

    Update data_types.py

---
+++ variables_02/data_types.py
@@ -0,0 +1,45 @@
+# Python Data Types Examples
+print("\n********* data types in Python: **********\n")
+# 1. Integer
+age = 17
+print("Integer:", age, type(age))
+print()
+
+# 2. Float
+height = 5.8
+print("Float:", height, type(height))
+print()
+
+# 3. String
+name = "Muhammad Asif"
+print("String:", name, type(name))
+print()
+
+# 4. Boolean
+is_student = True
+print("Boolean:", is_student, type(is_student))
+print()
+
+# 5. List
+fruits = ["apple", "banana", "mango", 20]
+print("List:", fruits, type(fruits))
+print()
+
+# 6. Tuple
+coordinates = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130)
+print("Tuple:", coordinates, type(coordinates))
+print()
+
+# 7. Set
+unique_numbers = {1, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 9, 10}
+print("Set:", unique_numbers, type(unique_numbers))
+print()
+
+# 8. Dictionary
+person = {"name": "Asif", "age": 20, "city": "Peshawar"}
+print("Dictionary:", person, type(person))
+print()
+
+# 9. NoneType
+nothing = None
+print("NoneType:", nothing, type(nothing))
