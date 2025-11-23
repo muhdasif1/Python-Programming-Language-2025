4328e68e4bd1575642a35673b199bf3808bbb1ab
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-11-23 20:52:02 +0500

    Update milk_calculation.py

--- PROJECTS/milk_calculation.py
+++ PROJECTS/milk_calculation.py
@@ -21,4 +21,10 @@ total_staff: int = 4
 total_absent: int = 15
 milk_per_person: int = 250
 
-print(f"Total Students: {total_students} \nFaculty Members: {faculty_members} \nTotal Staff: {total_staff} \nTotal Absent: {total_absent} \nMilk Per Person: {milk_per_person} \nTotal Members: {total_students + faculty_members + total_staff - total_absent} \nTotal Milk: {(total_students + faculty_members + total_staff - total_absent) * milk_per_person}")
+print(f"""Total Students: {total_students} 
+      \nFaculty Members: {faculty_members} 
+      \nTotal Staff: {total_staff} 
+      \nTotal Absent: {total_absent} 
+      \nMilk Per Person: {milk_per_person} 
+      \nTotal Members: {total_students + faculty_members + total_staff - total_absent} 
+      \nTotal Milk: {(total_students + faculty_members + total_staff - total_absent) * milk_per_person}""")
