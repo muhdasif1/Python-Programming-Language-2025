42b960c2c333d9d156123dd6e48ce039dd603a4f
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-12-19 19:54:49 +0500

    Create 03_data_types.py

---
+++ introduction_01/03_data_types.py
@@ -0,0 +1,24 @@
+number1 : int = 10 # integer type
+number2 : float = 15.5 # float type
+complex_number : complex = 2 + 3j # complex type
+boolean_value : bool = True # boolean type
+text : str = "Hello, World!" # string type
+mixed_text : str = "Hello123" # mixed string type
+num_text : str = "12345" # numeric string type
+list_example : list = [1, 2, 3, 4, 5] # list type
+tuple_example : tuple = (1, 2, 3) # tuple type
+set_example : set = {1, 2, 3} # set type
+dict_example : dict = {"key1": "value1", "key2": "value2"} # dictionary type
+print("Integer:", number1)
+print("Float:", number2)
+print("Complex Number:", complex_number)
+print("Boolean Value:", boolean_value)
+print("String:", text)
+print("Mixed String:", mixed_text)
+print("Numeric String:", num_text)
+print("List:", list_example)
+print("Tuple:", tuple_example)
+print("Set:", set_example)
+print("Dictionary:", dict_example)
+# ============================================================
+
