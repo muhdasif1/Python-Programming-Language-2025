fd13189a39c1283e16753eb3d2ff621afd470f99
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-12-19 20:46:35 +0500

    Create 03_comparison_operators.py

---
+++ 02_Operators/03_comparison_operators.py
@@ -0,0 +1,25 @@
+# ========================================
+# Python Comparison Operators
+# ========================================
+
+# Declare two numbers
+a = 10
+b = 5
+
+# Equal to (==)
+print("a == b:", a == b)  # False
+
+# Not equal to (!=)
+print("a != b:", a != b)  # True
+
+# Greater than (>)
+print("a > b:", a > b)    # True
+
+# Less than (<)
+print("a < b:", a < b)    # False
+
+# Greater than or equal to (>=)
+print("a >= b:", a >= b)  # True
+
+# Less than or equal to (<=)
+print("a <= b:", a <= b)  # False
