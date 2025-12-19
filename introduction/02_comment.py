1bc51e3fc96dc5f8c9d493bb24d1f67a1dcef202
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-12-19 20:56:22 +0500

    Delete 02_comment.py

--- introduction/02_comment.py
+++
@@ -1,9 +0,0 @@
-# This is a single-line comment
-# Python ignores this line
-# Used to explain code
-
-"""
-This is a multi-line comment
-It can span multiple lines
-Used for documentation or explanation
-"""
