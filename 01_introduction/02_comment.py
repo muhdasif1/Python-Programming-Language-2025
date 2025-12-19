92b7ae5a652cc56094897ef21cbf85bebb27ec52
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2025-12-19 20:54:03 +0500

    Create 02_comment.py

---
+++ 01_introduction/02_comment.py
@@ -0,0 +1,9 @@
+# This is a single-line comment
+# Python ignores this line
+# Used to explain code
+
+"""
+This is a multi-line comment
+It can span multiple lines
+Used for documentation or explanation
+"""
