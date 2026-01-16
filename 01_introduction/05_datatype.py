90470956f66697529ec7b53ef00d4929ef88d76a
muhdasif1 <asifnasaaz2005@gmail.com>
2026-01-16 22:14:24 +0500

    Update 05_datatype.py

--- 01_introduction/05_datatype.py
+++ 01_introduction/05_datatype.py
@@ -1,45 +1,52 @@
 # Text Type
 name = "Python"
 print(name, type(name))  # Output: <class 'str'>
+print()
 
 # Numeric Types
-x = 10          # int
-y = 3.14        # float
-z = 2 + 3j      # complex
-print(x, type(x))
-print(y, type(y))
-print(z, type(z))
+number1: int = 10          # int
+number2: float = 3.14        # float
+number3: complex = 2 + 3j      # complex
+print(number1, type(number1))
+print(number2, type(number2))
+print(number3, type(number3))
+print()
 
 # Sequence Types
-fruits = ["apple", "banana", "mango"]  # list
-colors = ("red", "green", "blue")      # tuple
-numbers = range(1, 5)                  # range
+fruits: list = ["apple", "banana", "mango"]  # list
+colors: tuple = ("red", "green", "blue")      # tuple
+numbers: range = range(1, 5)                  # range
 print(fruits, type(fruits))
 print(colors, type(colors))
 print(list(numbers), type(numbers))  # range converted to list for display
+print()
 
 # Mapping Type
-student = {"name": "Asif", "age": 20}  # dict
+student: dict = {"name": "Asif", "age": 20}  # dict
 print(student, type(student))
+print()
 
 # Set Types
-unique_numbers = {1, 2, 3}             # set
-fs = frozenset([1, 2, 3])              # frozenset
+unique_numbers: set = {1, 2, 3}             # set
+fs: frozenset = frozenset([1, 2, 3])              # frozenset
 print(unique_numbers, type(unique_numbers))
 print(fs, type(fs))
+print()
 
 # Boolean Type
-is_active = True                       # bool
+is_active: bool = True                       # bool
 print(is_active, type(is_active))
+print()
 
 # Binary Types
-b = bytes(5)                           # bytes
-ba = bytearray(5)                      # bytearray
-mv = memoryview(bytes(5))              # memoryview
+b: bytes = bytes(5)        # bytes
+ba: bytearray = bytearray(5)                      # bytearray
+mv: memoryview = memoryview(bytes(5))              # memoryview
 print(b, type(b))
 print(ba, type(ba))
 print(mv, type(mv))
+print()
 
 # None Type
-x = None
+x: None = None
 print(x, type(x))
