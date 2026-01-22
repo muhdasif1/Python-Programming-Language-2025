02a3772bbe3e29860ba0b6f6c1a4b1b585c4bc84
muhdasif1 <asifnasaaz2005@gmail.com>
2026-01-22 16:55:43 +0500

    Create 09_copy_list.py

---
+++ 03_Lists/09_copy_list.py
@@ -0,0 +1,14 @@
+# Copy a list using copy() method
+fruits = ["apple", "banana", "cherry"]
+fruits_copy = fruits.copy()
+print(fruits_copy)
+
+# Copy a list using list() method
+fruits = ["apple", "banana", "cherry"]
+fruits_copy = list(fruits)
+print(fruits_copy)
+
+# Copy a list using slice operator
+fruits = ["apple", "banana", "cherry"]
+fruits_copy = fruits[:]
+print(fruits_copy)
