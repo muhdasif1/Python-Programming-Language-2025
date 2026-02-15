8ad65aba9042beafab1fc3a998665d5dbfd7dc08
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-15 15:47:40 +0500

    Update 01_list.py

--- 03_Lists/01_list.py
+++ 03_Lists/01_list.py
@@ -1,25 +1,25 @@
 # Creating and printing a list
-fruits = ["apple", "banana", "cherry"]
-print(fruits)
+name: list[str] = ["Muhammad", "Asif", "Nouroz", "Moeez"]
+print(name)
 
 # Another list example
-this_list = ["apple", "banana", "cherry"]
+this_list : list[str] = ["Apple", "Banana", "Cherry","Orange", "Mango"]
 print(this_list)
 
 # List with duplicate values
-fruits_with_duplicates = ["apple", "banana", "cherry", "apple", "cherry"]
+fruits_with_duplicates : list[str] = ["apple", "banana", "cherry", "apple", "cherry"]
 print(fruits_with_duplicates)
 
 # Finding the length of a list
-this_list2 = ["apple", "banana", "cherry"]
+this_list2 :list[str] = ["apple", "banana", "cherry"]
 print(len(this_list2))
 
 # Lists with different data types
-string_list = ["apple", "banana", "cherry"]
+string_list: list[str] = ["apple", "banana", "cherry"]
 print(string_list)
-number_list = [1, 5, 7, 9, 3]
+number_list: list[int] = [1, 5, 7, 9, 3]
 print(number_list)
-boolean_list = [True, False, False]
+boolean_list : list[bool] = [True, False, False]
 print(boolean_list)
 
 # Mixed data type list
