69136af9185cd98240194399267dffab39724350
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-19 00:59:37 +0500

    Update 05_remove_list_item.py

--- 03_Lists/05_remove_list_item.py
+++ 03_Lists/05_remove_list_item.py
@@ -1,29 +1,35 @@
-# Remove specified item
+print("Remove specified item")
 fruits = ["apple", "banana", "cherry"]
 fruits.remove("banana")
 print(fruits)
+print("*****************************************************")
 
-# Remove first occurrence of a value
+print("Remove first occurrence of a value")
 fruits = ["apple", "banana", "cherry", "banana", "kiwi"]
 fruits.remove("banana")
 print(fruits)
+print("*****************************************************")
 
-# Remove item by index using pop()
+print("Remove item by index using pop()")
 fruits = ["apple", "banana", "cherry"]
 fruits.pop(1)
 print(fruits)
+print("*****************************************************")
 
-# Remove last item using pop()
+print("Remove last item using pop()")
 fruits = ["apple", "banana", "cherry"]
 fruits.pop()
 print(fruits)
+print("*****************************************************")
 
-# Remove item by index using del
+print("Remove item by index using del")
 fruits = ["apple", "banana", "cherry"]
 del fruits[0]
 print(fruits)
+print("*****************************************************")
 
-# Clear the list
+print("Clear the list")
 fruits = ["apple", "banana", "cherry"]
 fruits.clear()
 print(fruits)
+print("*****************************************************")
