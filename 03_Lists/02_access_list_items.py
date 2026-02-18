dd16f5683ad726f5f4dce8232594e8f7bf800a69
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-19 00:49:41 +0500

    Update 02_access_list_items.py

--- 03_Lists/02_access_list_items.py
+++ 03_Lists/02_access_list_items.py
@@ -1,23 +1,33 @@
-# Accessing an item by index (positive indexing)
+print("Accessing an item by index (positive indexing)")
 items = ["pen", "pencil", "eraser"]
+print(ite6ms[0])
 print(items[1])   # Output: pencil
+print(items[2])
+print("*****************************************************")
 
-# Accessing an item using negative indexing
+print("Accessing an item using negative indexing")
 items = ["pen", "pencil", "eraser"]
 print(items[-1])  # Output: eraser
+print(items[-2])
+print(items[-3])
+print("*****************************************************")
 
-# Accessing a range of items (index 2 to 4)
+print("Accessing a range of items (index 2 to 4)")
 items = ["pen", "pencil", "eraser", "marker", "sharpener", "ruler", "notebook"]
 print(items[2:5])  # Output: ['eraser', 'marker', 'sharpener']
+print("*****************************************************")
 
-# Accessing items from the beginning to index 3
+print("Accessing items from the beginning to index 3")
 items = ["pen", "pencil", "eraser", "marker", "sharpener", "ruler", "notebook"]
 print(items[:4])   # Output: ['pen', 'pencil', 'eraser', 'marker']
+print("*****************************************************")
 
-# Accessing items from index 2 to the end
+print("Accessing items from index 2 to the end")
 items = ["pen", "pencil", "eraser", "marker", "sharpener", "ruler", "notebook"]
 print(items[2:])   # Output: ['eraser', 'marker', 'sharpener', 'ruler', 'notebook']
+print("*****************************************************")
 
-# Accessing items using negative range indexing
+print("Accessing items using negative range indexing")
 items = ["pen", "pencil", "eraser", "marker", "sharpener", "ruler", "notebook"]
 print(items[-4:-1])  # Output: ['marker', 'sharpener', 'ruler']
+print("*****************************************************")
