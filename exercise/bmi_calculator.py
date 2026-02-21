67ec23e955f98de436888ea4636b3d043ed1ffeb
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-21 05:48:35 +0500

    Update bmi_calculator.py

--- exercise/bmi_calculator.py
+++ exercise/bmi_calculator.py
@@ -1,37 +1,33 @@
 """ 
 🧮 Python Assignment: BMI Calculator
 
-Objective:
-Use variables and mathematical operators in Python to calculate a person’s Body Mass Index (BMI).
+> Objective:
+- Use variables and mathematical operators in Python to calculate a person’s Body Mass Index (BMI).
 
-Task Description:
-The Body Mass Index (BMI) is a measure used in medicine to check if someone is underweight or overweight. The formula to calculate BMI is:
+> Task Description:
+- The Body Mass Index (BMI) is a measure used in medicine to check if someone is underweight or overweight. 
+The formula to calculate BMI is:
+- BMI = weight / (height × height)
 
-BMI = weight / (height × height)
+> Instructions:
+1 Create a new Python file named bmi_calculator.ipynb.
+2 Aprogram.dd a comment at the top describing the purpose of your
+3 Ask the user to enter their weight in kilograms and height in meters.
+4 Convert this sentence into Python code:
+- “BMI is equal to the person's weight divided by the person's height squared.”
+5 Display the calculated BMI on the screen.
 
-Instructions:
-Create a new Python file named bmi_calculator.ipynb.
+> Example Output (for reference only):
+- Enter your weight in kilograms: 60
+- Enter your height in meters: 1.65
+- Your BMI is: 22.03
 
-Aprogram.dd a comment at the top describing the purpose of your
 
-Ask the user to enter their weight in kilograms and height in meters.
-
-Convert this sentence into Python code:
-
-“BMI is equal to the person's weight divided by the person's height squared.”
-
-Display the calculated BMI on the screen.
-
-Example Output (for reference only):
-Enter your weight in kilograms: 60
-Enter your height in meters: 1.65
-Your BMI is: 22.03
-Bonus Challenge (Optional):
-After calculating the BMI, display a message showing whether the person is:
-
-Underweight
-Normal weight
-Overweight
+> Bonus Challenge (Optional):
+. After calculating the BMI, display a message showing whether the person is:
+- Underweight
+- Normal weight
+- Overweight
 """
 
 
