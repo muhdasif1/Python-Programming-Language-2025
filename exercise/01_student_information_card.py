caae1e26e8c5b704f2ef756d3472179ebe6f4291
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-21 05:17:25 +0500

    Create 01_student_information_card.py

---
+++ exercise/01_student_information_card.py
@@ -0,0 +1,29 @@
+""" 
+🧾 Use Case 1: Student Information Card:
+Welcome to your first Python practice assignment! 
+You’ll complete two simple, real-world programs using the topics you learned today:
+
+Variables
+Data Types (int, float, str)
+The print() Function
+
+1. Create variables for:
+student_name (string)
+student_age (integer)
+student_grade (string)
+student_gpa (float)
+"""
+
+student_name : str = "Muhammad Asif"
+student_age : int = 23
+student_grade : str = "7th"
+student_gpa : float = 3.2
+
+print("Student Information Card")
+print(f"Student Name: {student_name}")
+print(f"Student Age: {student_age}")
+print(f"Student Grade: {student_grade}")
+print(f"Student GPA: {student_gpa}")
+print()
+print(student_name, "is", student_age, "years old and currently in", student_grade, "grade.")
+
