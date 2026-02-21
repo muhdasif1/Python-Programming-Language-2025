b85d8618cd83fbbc3c310d7b9b9b4fcf387be905
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-02-21 06:04:44 +0500

    Update bmi_calculator.py

--- exercise/bmi_calculator.py
+++ exercise/bmi_calculator.py
@@ -1,14 +1,13 @@
 # 🧮 BMI Calculator
 # This program calculates a person's Body Mass Index (BMI)
 # using their weight (kg) and height (meters).
-
 # Step 1: Ask user for input
 # Step 2: Calculate BMI
 # BMI is equal to the person's weight divided by the person's height squared.
 # Step 3: Display the result (rounded to 2 decimal places)
 bmi = weight / (height * height)
 print("Your BMI is:", round(bmi, 2))
-# 🎯 Bonus Challenge: BMI Category
+
 if bmi < 18.5:
     print("Category: Underweight")
 elif bmi >= 18.5 and bmi < 25:
