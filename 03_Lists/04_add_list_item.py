a25faee3872f83ed87160ab68285a9360fdee6d4
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-04-16 04:03:58 +0500

    Update 04_add_list_item.py

--- 03_Lists/04_add_list_item.py
+++ 03_Lists/04_add_list_item.py
@@ -3,6 +3,7 @@ print("Append item")
 thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
 print(thislist)
+print()
 
 # Insert item
 print("Insert Item")
@@ -13,6 +14,7 @@ print(thislist)
 car : list = ["Toyota", "RangeRover", "Hundai", "Honda", "Mercedes"]
 car.insert(1,"BMW")
 print(car) 
+print()
 
 # Extend list with another list
 print("Extend  List")
@@ -20,8 +22,10 @@ thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
+print()
 
 name1: list = ["Muhammad asif", "moeez khan", "Nouroz"]
 name2: list = ["Usman", "Ahmad", "Raza", "Shery"]
 name1.extend(name2)
 print(name1)
+print()
