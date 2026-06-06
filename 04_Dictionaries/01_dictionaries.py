d2108195f792dc2bda1c089f69cf401fcb19a3d1
Muhammad Asif <140275007+muhdasif1@users.noreply.github.com>
2026-06-06 15:34:31 +0500

    Update 01_dictionaries.py

--- 04_Dictionaries/01_dictionaries.py
+++ 04_Dictionaries/01_dictionaries.py
@@ -1,3 +1,10 @@
+data1 = {
+  "Name": "Muhammad Asif",
+  "Father Name": "Gul Nawaz",
+  "Age": 22
+}
+print(data1)
+
 
 data2 = {
   "Student Name": "Muhammad Asif",
@@ -5,4 +12,14 @@ data2 = {
   "Department": "BS Computer Science",
   "year": 2022
 }
-print(data2["Student Name"])
+print(data2)
+
+
+# Python Dictionary DUPLICATE NOT ALLOWED
+data3 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(data3)
